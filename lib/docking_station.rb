220e7104310907aaa4b79c48fb025733161cdfeb
William Muxworthy <williammuxworthy@Williams-MacBook-Pro.local>
2018-10-02 16:34:26 -0700

    step11 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,27 @@
-require 'bike'
+require_relative 'bike'
 
 class DockingStation
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
     while true
       bike = Bike.new
       return bike if bike.working?
     end
+  end
+
+  def dock(bike)
+    @bikes << bike
+  end
+
+  def examine_bikes(selection)
+    # puts "There are #{@bikes.length} bike(s) available"
+    # puts "Which bike would you like to see?"
+    @bikes[selection]
+
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,4 +6,21 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it 'should dock bike' do
+  bike = Bike.new
+  subject.dock(bike)
+  expect(subject.bikes).to eq([bike])
+  end
+
+  it 'should examine bike' do
+    bike = Bike.new
+
+    subject.dock(bike)
+    selected_bike = subject.examine_bikes(0)
+    expect(selected_bike).to eq(bike)
+  end
+
+
+
 end
