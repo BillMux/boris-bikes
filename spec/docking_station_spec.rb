556f5313407e8c32a53ed6f0efd3defe62904d4e
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-01-17 11:14:36 +0000

    Attempt before block, 3 tests fail

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,26 +1,30 @@
 require 'docking_station'
 require 'bike'
+
 describe DockingStation do
+
+  before(:each) do
+    bike = Bike.new
+    subject.dock(bike)
+  end
+
   it { is_expected.to respond_to :release_bike }
+
   it 'should return bike on release_bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    expect(subject.release_bike).to eq(bike)
   end
 
   it 'should dock bike' do
-  bike = Bike.new
-  subject.dock(bike)
-  expect(subject.bikes).to eq([bike])
+    expect(subject.bikes).to eq([bike])
   end
 
   it 'should examine bike' do
-    bike = Bike.new
-
-    subject.dock(bike)
     selected_bike = subject.examine_bikes(0)
     expect(selected_bike).to eq(bike)
   end
 
-
+  # it 'does not release broken bikes' do
+  #   broken_bike =
+  # end
 
 end
