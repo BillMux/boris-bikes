f0146d1ff6dbb922ce261aca902d61e2c8de098c
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-01-17 11:14:58 +0000

    Delete empty lines

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -19,9 +19,6 @@ class DockingStation
   end
 
   def examine_bikes(selection)
-    # puts "There are #{@bikes.length} bike(s) available"
-    # puts "Which bike would you like to see?"
     @bikes[selection]
-
   end
 end
